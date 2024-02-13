7d1fbf52f9230244990964497f03bcd763f6acf2
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-13 10:56:32 +0000

    wip

--- rinhadebackend/publish-image.sh
+++ rinhadebackend/publish-image.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -f "$DIR/src/main/docker/Dockerfile" --no-cache --progress=plain -t caravanacloud/rinhadebackend:latest "$DIR"
 # Remember to docker login
+echo "## Building Default image"
+docker build -f "$DIR/src/main/docker/Dockerfile" --no-cache --progress=plain -t caravanacloud/rinhadebackend:latest "$DIR"
 docker push caravanacloud/rinhadebackend:latest
+
+echo "## Building JVM image"
+docker build -f "$DIR/src/main/docker/Dockerfile.jvm" --no-cache --progress=plain -t caravanacloud/rinhadebackend:latest "$DIR"
+docker push caravanacloud/rinhadebackend-jvm:latest
+
+echo "## Building Native image"
+docker build -f "$DIR/src/main/docker/Dockerfile.native" --no-cache --progress=plain -t caravanacloud/rinhadebackend:latest "$DIR"
+docker push caravanacloud/rinhadebackend-native:latest
