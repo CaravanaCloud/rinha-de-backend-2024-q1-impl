11fb16f88111d89faab97bebf84adf5aeb65a394
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-15 15:20:37 +0000

    wip

---
+++ publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp docker-compose.yml nginx.conf postgresql.conf init.sql ./rinha-de-backend-2024-q1/participantes/caravanacloud/
+rm -f ./rinha-de-backend-2024-q1/participantes/caravanacloud/testada
+rm -f ./rinha-de-backend-2024-q1/participantes/caravanacloud/*.log
--- rinhadebackend/publish-image.sh
+++ rinhadebackend/publish-image.sh
@@ -4,7 +4,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "## Checking registry authentication"
 docker login
 
-# Remember to docker login
+
+echo "## Building Native image"
+docker build -f "$DIR/src/main/docker/Dockerfile.native" --no-cache --progress=plain -t caravanacloud/rinhadebackend-native:latest "$DIR"
+docker push caravanacloud/rinhadebackend-native:latest
+
 echo "## Building Default image"
 docker build -f "$DIR/src/main/docker/Dockerfile" --no-cache --progress=plain -t caravanacloud/rinhadebackend:latest "$DIR"
 docker push caravanacloud/rinhadebackend:latest
@@ -13,8 +17,5 @@ echo "## Building JVM image"
 docker build -f "$DIR/src/main/docker/Dockerfile.jvm" --no-cache --progress=plain -t caravanacloud/rinhadebackend-jvm:latest "$DIR"
 docker push caravanacloud/rinhadebackend-jvm:latest
 
-echo "## Building Native image"
-docker build -f "$DIR/src/main/docker/Dockerfile.native" --no-cache --progress=plain -t caravanacloud/rinhadebackend-native:latest "$DIR"
-docker push caravanacloud/rinhadebackend-native:latest
 
 echo "## done"
