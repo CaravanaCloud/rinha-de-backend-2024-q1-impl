0343bb1666dc9d18fb2db17159c0dd54978245e2
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-16 14:25:32 +0000

    wip

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cp docker-compose.yml nginx.conf postgresql.conf init.sql ./rinha-de-backend-2024-q1/participantes/caravanacloud/
+cp ./rinhadebackend/src/main/resources/db/init.postgresql.sql .
+cp docker-compose.yml nginx.conf postgresql.conf init.postgresql.sql ./rinha-de-backend-2024-q1/participantes/caravanacloud/
 rm -f ./rinha-de-backend-2024-q1/participantes/caravanacloud/testada
 rm -f ./rinha-de-backend-2024-q1/participantes/caravanacloud/*.log
+
+echo done
