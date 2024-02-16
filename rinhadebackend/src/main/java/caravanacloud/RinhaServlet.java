45b86d949709b56846ac5c3a25415212e5e73918
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-16 12:32:14 +0000

    Why not Servlets :)

---
+++ rinhadebackend/src/main/java/caravanacloud/RinhaServlet.java
@@ -0,0 +1,26 @@
+package caravanacloud;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/*")
+public class RinhaServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
+    
+}
