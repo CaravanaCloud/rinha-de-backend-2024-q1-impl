14eb0795242c5ad0be553b3bed41c04e70f62f42
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-18 17:19:29 +0000

    warmupa

--- rinhadebackend/src/main/dead/RinhaServlet.java
+++ rinhadebackend/src/main/dead/RinhaServlet.java
@@ -108,7 +108,7 @@ public class RinhaServlet extends HttpServlet {
         }
     }
 
-    // curl -v -X POST -H "Content-Type: application/json" -d '{"valor": 100, "tipo": "c", "descricao": "Deposito"}' http:///localhost:9999/clientes/1/transacoes
+    // curl -v -X POST -H "Content-Type: application/json" -d '{"valor": 0, "tipo": "c", "descricao": "Deposito"}' http:///localhost:9999/clientes/1/transacoes
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var pathInfo = req.getPathInfo();
