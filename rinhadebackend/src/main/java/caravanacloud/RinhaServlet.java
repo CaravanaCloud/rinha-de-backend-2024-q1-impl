32b3bdb20eba09d43db34af4a42ba302652ef47c
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-18 01:34:13 +0000

    wip

--- rinhadebackend/src/main/java/caravanacloud/RinhaServlet.java
+++ rinhadebackend/src/main/java/caravanacloud/RinhaServlet.java
@@ -17,12 +17,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import static jakarta.servlet.http.HttpServletResponse.*;
+
 
 @WebServlet("/*")
 public class RinhaServlet extends HttpServlet {
     private static final String EXTRATO_QUERY = "select * from proc_extrato(?)";
     private static final String TRANSACAO_QUERY =  "select * from proc_transacao(?, ?, ?, ?)";
-
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Inject
@@ -66,24 +67,27 @@ public class RinhaServlet extends HttpServlet {
                 if (rs.next()) {
                     var result = rs.getString(1);
                     resp.setContentType("application/json");
-                    resp.setCharacterEncoding("UTF-8");
                     resp.getWriter().write(result);
                 } else {
-                    resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Extrato nao encontrado");
+                    sendError(resp, SC_NOT_FOUND, "Extrato nao encontrado");
                 }
             }
         } catch (SQLException e) {
             var msg = e.getMessage();
             if (msg.contains("CLIENTE_NAO_ENCONTRADO")) {
-                if (resp != null) resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Cliente nao encontrado");
+                sendError(resp, SC_NOT_FOUND, "Cliente nao encontrado");
             } else {
                 e.printStackTrace();
-                if (resp != null) resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro SQL ao processar a transacao");
+                sendError(resp, SC_INTERNAL_SERVER_ERROR, "Erro SQL ao processar a transacao");
             }
         } catch (Exception e) {
             e.printStackTrace();
-            if (resp != null) resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao processar a transacao");
+            sendError(resp, SC_INTERNAL_SERVER_ERROR, "Erro ao processar a transacao");
         }
+    }
+
+    private void sendError(HttpServletResponse resp, int sc, String msg) throws IOException{
+        sendError(resp, sc, msg);
     }
 
     // curl -v -X POST -H "Content-Type: application/json" -d '{"valor": 100, "tipo": "c", "descricao": "Deposito"}' http:///localhost:9999/clientes/1/transacoes
@@ -96,7 +100,7 @@ public class RinhaServlet extends HttpServlet {
         try (BufferedReader reader = req.getReader()) {
             json = objectMapper.readTree(reader);
         } catch (Exception e) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid request body");
+            sendError(resp, SC_BAD_REQUEST, "Invalid request body");
             return;
         }
         postTransacao(Integer.valueOf(id), json, resp);
@@ -107,7 +111,7 @@ public class RinhaServlet extends HttpServlet {
         // Validate and process the transaction as in the original resource
         var valorNumber = (Object) t.get("valor").asInt();
         if (valorNumber == null || !( valorNumber instanceof Integer)) {
-            if(resp != null) resp.sendError(422, "Valor invalido");
+            if(resp != null) sendError(resp, 422, "Valor invalido");
             return;
         }
         var valor = (Integer) valorNumber;
@@ -115,13 +119,13 @@ public class RinhaServlet extends HttpServlet {
 
         var tipo = (String) t.get("tipo").asText();
         if (tipo == null || !("c".equals(tipo) || "d".equals(tipo))) {
-            if(resp != null) resp.sendError(422, "Tipo invalido");
+            if(resp != null) sendError(resp, 422, "Tipo invalido");
             return;
         }
 
         var descricao = (String) t.get("descricao").asText();
         if (descricao == null || descricao.isEmpty() || descricao.length() > 10) {
-            if(resp != null) resp.sendError(422, "Descricao invalida");
+            if(resp != null) sendError(resp, 422, "Descricao invalida");
             return;
         }
 
@@ -146,24 +150,24 @@ public class RinhaServlet extends HttpServlet {
                     var output = objectMapper.writeValueAsString(body);
                     resp.getWriter().write(output);
                 } else {
-                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao processar a transacao");
+                    sendError(resp, SC_INTERNAL_SERVER_ERROR, "Erro ao processar a transacao");
                 }
             }
         } catch (SQLException e) {
             handleSQLException(e, resp);
         } catch (Exception e) {
-            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao processar a transacao");
+            sendError(resp, SC_INTERNAL_SERVER_ERROR, "Erro ao processar a transacao");
         }
     }
 
     private void handleSQLException(SQLException e, HttpServletResponse resp) throws IOException {
         var msg = e.getMessage();
         if (msg.contains("LIMITE_INDISPONIVEL")) {
-            if (resp != null) resp.sendError(422, "Erro: Limite indisponivel");
+            sendError(resp, 422, "Erro: Limite indisponivel");
         } else if (msg.contains("fk_clientes_transacoes_id")) {
-            if (resp != null) resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Erro: Cliente inexistente");
+            sendError(resp, SC_NOT_FOUND, "Erro: Cliente inexistente");
         } else {
-            if (resp != null) resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro SQL ao processar a transacao");
+            sendError(resp, SC_INTERNAL_SERVER_ERROR, "Erro SQL ao processar a transacao");
         }
     }
 }
