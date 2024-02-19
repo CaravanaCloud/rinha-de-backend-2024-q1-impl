fc0cb979be869c6b9db34491dc2535bd80eab54e
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-19 18:52:35 +0000

    wip

--- init.postgresql.sql
+++ init.postgresql.sql
@@ -11,7 +11,8 @@ BEGIN
             valor INTEGER NOT NULL,
             tipo CHAR(1) NOT NULL,
             descricao VARCHAR(255) NOT NULL,
-            realizada_em TIMESTAMP NOT NULL DEFAULT NOW()
+            realizada_em TIMESTAMP NOT NULL DEFAULT NOW(),
+            saldo INTEGER NOT NULL DEFAULT 0
         )', i);
         EXECUTE format('SELECT pg_prewarm(''transacoes_%s'')', i);
     END LOOP;
@@ -35,10 +36,10 @@ CREATE OR REPLACE FUNCTION proc_transacao(p_cliente_id INT, p_valor INT, p_tipo 
 RETURNS TABLE(saldo INT, limite INT) AS $$
 DECLARE
     diff INT;
-    v_saldo INT;
+    v_saldo_atual INT;
+    v_novo_saldo INT;
     v_limite INT;
     tabela_transacao TEXT := 'transacoes_' || p_cliente_id;
-    ultima_transacao RECORD;
 BEGIN
     -- Obtendo o limite para o cliente
     v_limite := obter_limite_cliente(p_cliente_id);
@@ -49,62 +50,66 @@ BEGIN
     ELSE
         diff := p_valor;
     END IF;
-    
-    EXECUTE format('LOCK TABLE %I IN EXCLUSIVE MODE', tabela_transacao);
 
-    -- Buscando a última transação para calcular o saldo atual
-    EXECUTE format('SELECT valor FROM %I ORDER BY id DESC LIMIT 1', tabela_transacao) INTO ultima_transacao;
+    -- Obtendo o saldo atual da última transação
+    EXECUTE format('SELECT saldo FROM %I ORDER BY id DESC LIMIT 1', tabela_transacao) INTO v_saldo_atual;
     IF NOT FOUND THEN
-        v_saldo := 0;
-    ELSE
-        v_saldo := ultima_transacao.valor;
+        v_saldo_atual := 0;
     END IF;
 
     -- Calculando o novo saldo
-    v_saldo := v_saldo + diff;
+    v_novo_saldo := v_saldo_atual + diff;
 
     -- Verificando se a nova transação excede o limite
-    IF v_saldo < (-1 * v_limite) THEN
-        RAISE EXCEPTION 'Limite ultrapassado. Saldo: %, Limite: %', v_saldo, v_limite;
+    IF v_novo_saldo < (-1 * v_limite) THEN
+        RAISE EXCEPTION 'Limite ultrapassado. Saldo: %, Limite: %', v_novo_saldo, v_limite;
     END IF;
 
-    -- Inserindo a nova transação se não exceder o limite
-    EXECUTE format('INSERT INTO %I (valor, tipo, descricao) VALUES (%L, %L, %L)', tabela_transacao, diff, p_tipo, p_descricao);
+    -- Inserindo a nova transação com o novo saldo atualizado
+    EXECUTE format('INSERT INTO %I (valor, tipo, descricao, saldo) VALUES (%L, %L, %L, %L)', tabela_transacao, diff, p_tipo, p_descricao, v_novo_saldo);
 
-    -- Retornando o saldo e limite atualizados
-    RETURN QUERY SELECT v_saldo, v_limite;
+    -- Retornando o novo saldo e limite
+    RETURN QUERY SELECT v_novo_saldo, v_limite;
 END;
 $$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE FUNCTION proc_extrato(p_cliente_id INTEGER)
-RETURNS json AS $$
+CREATE OR REPLACE FUNCTION proc_transacao(p_cliente_id INT, p_valor INT, p_tipo VARCHAR, p_descricao VARCHAR)
+RETURNS TABLE(saldo INT, limite INT) AS $$
 DECLARE
-    v_saldo INTEGER;
-    v_limite INTEGER; -- Variável para armazenar o limite
+    diff INT;
+    v_saldo_atual INT;
+    v_novo_saldo INT;
+    v_limite INT;
     tabela_transacao TEXT := 'transacoes_' || p_cliente_id;
-    transacoes json := '[]';
 BEGIN
-    -- Obtendo o limite para o cliente usando a função obter_limite_cliente
+    -- Obtendo o limite para o cliente
     v_limite := obter_limite_cliente(p_cliente_id);
 
-    -- Obtendo o saldo atual baseado na última transação
-    EXECUTE format('SELECT valor FROM %I ORDER BY id DESC LIMIT 1', tabela_transacao) INTO v_saldo;
-    IF NOT FOUND THEN
-        v_saldo := 0; -- Considera saldo zero se não houver transações
+    -- Determinando o valor da transação (positivo para créditos, negativo para débitos)
+    IF p_tipo = 'd' THEN
+        diff := p_valor * -1;
+    ELSE
+        diff := p_valor;
     END IF;
 
-    -- Obtendo as últimas transações para o cliente
-    EXECUTE format('SELECT json_agg(row_to_json(t.*)) FROM (SELECT valor, tipo, descricao, TO_CHAR(realizada_em, ''YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'') AS realizada_em FROM %I ORDER BY realizada_em DESC LIMIT 10) t', tabela_transacao) INTO transacoes;
+    -- Obtendo o saldo atual da última transação
+    EXECUTE format('SELECT saldo FROM %I ORDER BY id DESC LIMIT 1', tabela_transacao) INTO v_saldo_atual;
+    IF NOT FOUND THEN
+        v_saldo_atual := 0;
+    END IF;
 
-    -- Construindo e retornando o JSON de resultado
-    RETURN json_build_object(
-        'saldo', json_build_object(
-            'total', v_saldo,
-            'data_extrato', TO_CHAR(NOW(), 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'),
-            'limite', v_limite
-        ),
-        'ultimas_transacoes', COALESCE(transacoes, '[]')
-    );
+    -- Calculando o novo saldo
+    v_novo_saldo := v_saldo_atual + diff;
+
+    -- Verificando se a nova transação excede o limite
+    IF v_novo_saldo < (-1 * v_limite) THEN
+        RAISE EXCEPTION 'Limite ultrapassado. Saldo: %, Limite: %', v_novo_saldo, v_limite;
+    END IF;
+
+    -- Inserindo a nova transação com o novo saldo atualizado
+    EXECUTE format('INSERT INTO %I (valor, tipo, descricao, saldo) VALUES (%L, %L, %L, %L)', tabela_transacao, diff, p_tipo, p_descricao, v_novo_saldo);
+
+    -- Retornando o novo saldo e limite
+    RETURN QUERY SELECT v_novo_saldo, v_limite;
 END;
 $$ LANGUAGE plpgsql;
