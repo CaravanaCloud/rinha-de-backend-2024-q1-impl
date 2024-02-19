19ab31a44cb60e52f418913d5207884980ea2ce2
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-19 22:37:32 +0000

    wip

--- init.postgresql.1table.sql
+++ init.postgresql.1table.sql
@@ -9,6 +9,14 @@ CREATE UNLOGGED TABLE IF NOT EXISTS transacoes (
     saldo INTEGER NOT NULL DEFAULT 0
 );
 
+insert into transacoes (cliente_id, valor, tipo, descricao, saldo)
+    values 
+    (1, 0, 'c', 'Deposito inicial', 0),
+    (2, 0, 'c', 'Deposito inicial', 0),
+    (3, 0, 'c', 'Deposito inicial', 0),
+    (4, 0, 'c', 'Deposito inicial', 0),
+    (5, 0, 'c', 'Deposito inicial', 0);
+
 -- Preparando o ambiente
 CREATE EXTENSION IF NOT EXISTS pg_prewarm;
 SELECT pg_prewarm('transacoes');
