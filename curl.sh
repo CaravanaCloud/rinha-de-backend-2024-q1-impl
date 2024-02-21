196ab9e5c2f8cf08e73f7aa83c080af063a924f2
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-21 22:02:23 +0000

    user-defined pgo

--- curl.sh
+++ curl.sh
@@ -4,4 +4,4 @@ curl -v -X POST -H "Content-Type: application/json" -d '{"valor": 0,"tipo": "c",
 
 curl -v -X POST -H "Content-Type: application/json" -d '{"valor": 0,"tipo": "c", "descricao": ""}' http:///localhost:9999/clientes/1/transacoes
 curl -v -X POST -H "Content-Type: application/json" -d '{"valor": 0,"tipo": "c", "descricao": null}' http:///localhost:9999/clientes/1/transacoes
-curl -v -X POST -H "Content-Type: application/json" -d '{"valor": 1.2,"tipo": "c", "descricao": "teste"}' http:///localhost:9999/clientes/1/transacoes
+curl -v -X POST -H "Content-Type: application/json" -d '{"valor": 0,"tipo": "c", "descricao": "teste"}' http:///localhost:9999/clientes/1/transacoes
