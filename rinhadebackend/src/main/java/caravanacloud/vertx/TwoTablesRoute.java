777e94b681d0c485b22e83d6a44459ab83caf028
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-02-24 18:18:27 +0000

    tuned connection pool size

--- rinhadebackend/src/main/java/caravanacloud/vertx/TwoTablesRoute.java
+++ rinhadebackend/src/main/java/caravanacloud/vertx/TwoTablesRoute.java
@@ -24,10 +24,11 @@ import jakarta.ws.rs.core.Response;
 @ApplicationScoped
 @Path("/clientes")
 public class TwoTablesRoute {
-    private static final String VERSION_ID = "vertx-2tables-0.0.1";
+    private static final String VERSION_ID = "vertx-2tables-0.0.2";
     private static final String EXTRATO_QUERY = "select * from proc_extrato($1)";
     private static final String TRANSACAO_QUERY = "select * from proc_transacao($1, $2, $3, $4, $5)";
     private static final String WARMUP_QUERY = "select 1+1;";
+    private static final int WARMUP_LEVEL = 10;
 
     private int shard;
 
@@ -36,15 +37,18 @@ public class TwoTablesRoute {
 
     @Inject
     PgPool client; 
+
     public void onStartup(@Observes StartupEvent event) {
         shard = envInt("RINHA_SHARD", 0);
         System.out.println("StartupEvent shard["+ shard +"] version["+VERSION_ID+"] 🐔💥");
         boolean ready = false;
         do {
             try {
-                warmup();
-                processExtrato(333);
-                processTransacao(333, Map.of("valor", "0", "tipo", "c", "descricao", "warmup"));
+                for (int i = 0; i < WARMUP_LEVEL; i++) {
+                    warmup();
+                    processExtrato(333);
+                    processTransacao(333, Map.of("valor", "0", "tipo", "c", "descricao", "warmup"));                    
+                }
                 ready = true;
             } catch (Exception e) {
                 System.err.println("Warmup failed, waiting for db: " + e.getMessage());
@@ -58,33 +62,20 @@ public class TwoTablesRoute {
         } while (!ready);
     }
 
-    private static int envInt(String varName, int defaultVal) {
-        var result = System.getenv(varName);
-        if (result == null) {
-            System.out.println("Env var " + varName + " not found, using default " + defaultVal);
-            return defaultVal;
-        }
-        try {
-            return Integer.parseInt(result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
-        }
-    }
 
     private void warmup() {
         var query = getEnv("RINHA_WARMUP_QUERY", WARMUP_QUERY);
         Log.info("Warmup query: " + query);
-        client.preparedQuery(query).execute()
-                .await().indefinitely();
+        client.preparedQuery(query)
+            .execute()
+            .await()
+            .indefinitely();
         Log.info("Warmup done");
     }
 
-    private static String getEnv(String varName, String defaultVal) {
-        var result = System.getenv(varName);
-        return result != null ? result : defaultVal;
-    }
-
+    private boolean invalid(int id) {
+        return  id > 5 || id < 1;
+	}
 
     @GET                                                                                   
     @Path("/{id}/extrato")
@@ -97,33 +88,13 @@ public class TwoTablesRoute {
         return resp;  
     }
 
-
-    private boolean invalid(int id) {
-        return  id > 5 || id < 1;
-	}
-
 	private Uni<Response> processExtrato(int id) {
-        // invoke EXTRATO_QUERY
         var query = client.preparedQuery(EXTRATO_QUERY).execute(Tuple.of(id));
         Uni<Response> result = query.onItem().transform(RowSet::iterator) 
             .onItem().transform(iterator -> iterator.hasNext() ? iterator.next() : null)
             .onItem().transform(r -> r != null ? responseOf(r) : null)
             .onFailure().recoverWithItem(e -> errorOf(e,"err_extrato")); 
         return result;
-    }
-
-    private Response errorOf(Throwable e, String key) {
-        return  Response.status(422).entity("{\""+key+"\": \""+e.getMessage()+"\"}").build();
-    }
-
-    private Response errorOf(String e, int status) {
-        return  Response.status(status).entity("{\"err\": \""+e+"\"}").build();
-    }
-
-    private Response responseOf(Row r) {
-        var status = r.getInteger("status_code");
-        var body = r.getJson("body");
-        return Response.status(status).entity(body).build();
     }
 
     @Path("/{id}/transacoes")
@@ -160,11 +131,47 @@ public class TwoTablesRoute {
         if (descricao == null || descricao.isEmpty() || descricao.length() > 10 || "null".equals(descricao)) {
             return Uni.createFrom().item(errorOf("descricao invalida", 422));
         }
-        var query = client.preparedQuery(TRANSACAO_QUERY).execute(Tuple.of(shard, id, valor, tipo, descricao));
+
+        var query = client.preparedQuery(TRANSACAO_QUERY)
+            .execute(Tuple.of(shard, id, valor, tipo, descricao));
+
         Uni<Response> result = query.onItem().transform(RowSet::iterator) 
             .onItem().transform(iterator -> iterator.hasNext() ? iterator.next() : null)
             .onItem().transform(r -> r != null ? responseOf(r) : null)
             .onFailure().recoverWithItem(e -> errorOf(e, "err_transacao"));  
         return result;
 	}
+
+    private static int envInt(String varName, int defaultVal) {
+        var result = System.getenv(varName);
+        if (result == null) {
+            System.out.println("Env var " + varName + " not found, using default " + defaultVal);
+            return defaultVal;
+        }
+        try {
+            return Integer.parseInt(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    private static String getEnv(String varName, String defaultVal) {
+        var result = System.getenv(varName);
+        return result != null ? result : defaultVal;
+    }
+
+    private Response errorOf(Throwable e, String key) {
+        return  Response.status(422).entity("{\""+key+"\": \""+e.getMessage()+"\"}").build();
+    }
+
+    private Response errorOf(String e, int status) {
+        return  Response.status(status).entity("{\"err\": \""+e+"\"}").build();
+    }
+
+    private Response responseOf(Row r) {
+        var status = r.getInteger("status_code");
+        var body = r.getJson("body");
+        return Response.status(status).entity(body).build();
+    }
 }
