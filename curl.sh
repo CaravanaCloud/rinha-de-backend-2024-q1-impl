c029e9100864683f5192b3788e4aacbf0e49b6a3
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-03-02 18:33:06 +0000

    wip

--- curl.sh
+++ curl.sh
@@ -2,7 +2,7 @@
 
 curl -s -X POST \
     -H "Content-Type: application/json" \
-    -d '{"valor": 1, "tipo": "c", "descricao": "teste"}' \
+    -d '{"valor": 10, "tipo": "d", "descricao": "teste"}' \
         http:///localhost:9999/clientes/1/transacoes | jq
 
 curl -s -X GET \
